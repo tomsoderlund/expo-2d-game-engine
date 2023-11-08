d3274ce4c17a32660d6fe0f5d123a271606201fd
Tom Söderlund <tom@tomsoderlund.com>
2023-11-08 15:44:00 +0100

    expo-processing

--- App.tsx
+++ App.tsx
@@ -1,44 +1,56 @@
 import React from 'react'
-import { GLView } from 'expo-gl'
-import Expo2DContext from 'expo-2d-context'
+import { ProcessingView } from 'expo-processing'
 
 export default class App extends React.Component {
-  render (): React.ReactElement {
+  render () {
     return (
-      <GLView style={{ flex: 1 }} onContextCreate={this.handleGLContextCreate} />
+      <ProcessingView
+        style={{ flex: 1 }}
+        sketch={this._sketch}
+      />
     )
   }
 
-  handleGLContextCreate = (gl: ExpoWebGLRenderingContext): void => {
-    const ctx = new Expo2DContext(gl)
-    ctx.translate(50, 200)
-    ctx.scale(4, 4)
-    // Head
-    ctx.fillStyle = 'grey'
-    ctx.fillRect(20, 40, 100, 100)
-    // Teeth
-    ctx.fillStyle = 'white'
-    ctx.fillRect(30, 100, 20, 30)
-    ctx.fillRect(60, 100, 20, 30)
-    ctx.fillRect(90, 100, 20, 30)
-    // Eyes
-    ctx.beginPath()
-    ctx.arc(50, 70, 18, 0, 2 * Math.PI)
-    ctx.arc(90, 70, 18, 0, 2 * Math.PI)
-    ctx.fill()
-    // Eye pupils
-    ctx.fillStyle = 'dodgerblue'
-    ctx.beginPath()
-    ctx.arc(50, 70, 8, 0, 2 * Math.PI)
-    ctx.arc(90, 70, 8, 0, 2 * Math.PI)
-    ctx.fill()
-    // Antenna
-    ctx.strokeStyle = 'black'
-    ctx.beginPath()
-    ctx.moveTo(70, 40)
-    ctx.lineTo(70, 30)
-    ctx.arc(70, 20, 10, 0.5 * Math.PI, 2.5 * Math.PI)
-    ctx.stroke()
-    ctx.flush()
+  _sketch = (p): void => {
+    p.setup = () => {
+      p.strokeWeight(7)
+    }
+
+    const harom = (ax: number, ay: number, bx: number, by: number, level: number, ratio: number): void => {
+      if (level <= 0) {
+        return
+      }
+
+      const vx = bx - ax
+      const vy = by - ay
+      const nx = p.cos(p.PI / 3) * vx - p.sin(p.PI / 3) * vy
+      const ny = p.sin(p.PI / 3) * vx + p.cos(p.PI / 3) * vy
+      const cx = ax + nx
+      const cy = ay + ny
+      p.line(ax, ay, bx, by)
+      p.line(ax, ay, cx, cy)
+      p.line(cx, cy, bx, by)
+
+      harom(
+        ax * ratio + cx * (1 - ratio),
+        ay * ratio + cy * (1 - ratio),
+        ax * (1 - ratio) + bx * ratio,
+        ay * (1 - ratio) + by * ratio,
+        level - 1,
+        ratio
+      )
+    }
+
+    p.draw = () => {
+      p.background(240)
+      harom(
+        p.width - 142,
+        p.height - 142,
+        142,
+        p.height - 142,
+        6,
+        (p.sin((0.0005 * Date.now()) % (2 * p.PI)) as number + 1) / 2
+      )
+    }
   }
 }
