18d6dfe45b8e0a241911d4e48bc824bfe53ecafb
Tom Söderlund <tom@tomsoderlund.com>
2023-11-09 22:06:15 +0100

    Expo2DContext as functional component

--- App.tsx
+++ App.tsx
@@ -1,76 +1,72 @@
-import React from 'react'
+import React, { useRef, useCallback } from 'react'
 import { GestureResponderEvent } from 'react-native'
 import { GLView } from 'expo-gl'
 import Expo2DContext, { Expo2dContextOptions } from 'expo-2d-context'
 
-export default class App extends React.Component {
-  ctx?: Expo2DContext
+const App = (): React.ReactElement => {
+  const ctxRef = useRef<Expo2DContext | null>(null)
 
-  constructor (props: any) {
-    super(props)
-    this.ctx = undefined
-  }
-
-  render (): React.ReactElement {
-    return (
-      <GLView
-        style={{ flex: 1 }}
-        onContextCreate={this.handleSetup}
-        onStartShouldSetResponder={() => true}
-        onResponderGrant={this.handleTouchPress}
-        onResponderRelease={this.handleTouchRelease}
-        onResponderMove={this.handleTouchMove}
-      />
-    )
-  }
-
-  handleTouchPress = (e: GestureResponderEvent): void => {
+  const handleTouchPress = (e: GestureResponderEvent): void => {
     console.log('handleTouchPress', e.nativeEvent.locationX, e.nativeEvent.locationY)
   }
 
-  handleTouchRelease = (e: GestureResponderEvent): void => {
+  const handleTouchRelease = (e: GestureResponderEvent): void => {
     console.log('handleTouchRelease', e.nativeEvent.locationX, e.nativeEvent.locationY)
   }
 
-  handleTouchMove = (e: GestureResponderEvent): void => {
+  const handleTouchMove = (e: GestureResponderEvent): void => {
     console.log('handleTouchMove', e.nativeEvent.locationX, e.nativeEvent.locationY)
   }
 
-  handleSetup = (gl: WebGLRenderingContext): void => {
-    this.ctx = new Expo2DContext(gl as unknown as number, undefined as unknown as Expo2dContextOptions)
-    this.draw()
+  const handleSetup = useCallback((gl: WebGLRenderingContext) => {
+    const ctx = new Expo2DContext(gl as unknown as number, undefined as unknown as Expo2dContextOptions)
+    ctxRef.current = ctx
+    draw(ctx)
+  }, [])
+
+  const draw = (ctx: Expo2DContext): void => {
+    ctx.translate(50, 200)
+    ctx.scale(4, 4)
+    // Head
+    ctx.fillStyle = 'grey'
+    ctx.fillRect(20, 40, 100, 100)
+    // Teeth
+    ctx.fillStyle = 'white'
+    ctx.fillRect(30, 100, 20, 30)
+    ctx.fillRect(60, 100, 20, 30)
+    ctx.fillRect(90, 100, 20, 30)
+    // Eyes
+    ctx.beginPath()
+    ctx.arc(50, 70, 18, 0, 2 * Math.PI)
+    ctx.arc(90, 70, 18, 0, 2 * Math.PI)
+    ctx.fill()
+    // Eye pupils
+    ctx.fillStyle = 'dodgerblue'
+    ctx.beginPath()
+    ctx.arc(50, 70, 8, 0, 2 * Math.PI)
+    ctx.arc(90, 70, 8, 0, 2 * Math.PI)
+    ctx.fill()
+    // Antenna
+    ctx.strokeStyle = 'black'
+    ctx.beginPath()
+    ctx.moveTo(70, 40)
+    ctx.lineTo(70, 30)
+    ctx.arc(70, 20, 10, 0.5 * Math.PI, 2.5 * Math.PI)
+    ctx.stroke()
+
+    ctx.flush()
   }
 
-  draw = (): void => {
-    if (this.ctx === undefined) return
-    this.ctx.translate(50, 200)
-    this.ctx.scale(4, 4)
-    // Head
-    this.ctx.fillStyle = 'grey'
-    this.ctx.fillRect(20, 40, 100, 100)
-    // Teeth
-    this.ctx.fillStyle = 'white'
-    this.ctx.fillRect(30, 100, 20, 30)
-    this.ctx.fillRect(60, 100, 20, 30)
-    this.ctx.fillRect(90, 100, 20, 30)
-    // Eyes
-    this.ctx.beginPath()
-    this.ctx.arc(50, 70, 18, 0, 2 * Math.PI)
-    this.ctx.arc(90, 70, 18, 0, 2 * Math.PI)
-    this.ctx.fill()
-    // Eye pupils
-    this.ctx.fillStyle = 'dodgerblue'
-    this.ctx.beginPath()
-    this.ctx.arc(50, 70, 8, 0, 2 * Math.PI)
-    this.ctx.arc(90, 70, 8, 0, 2 * Math.PI)
-    this.ctx.fill()
-    // Antenna
-    this.ctx.strokeStyle = 'black'
-    this.ctx.beginPath()
-    this.ctx.moveTo(70, 40)
-    this.ctx.lineTo(70, 30)
-    this.ctx.arc(70, 20, 10, 0.5 * Math.PI, 2.5 * Math.PI)
-    this.ctx.stroke()
-    this.ctx.flush()
-  }
+  return (
+    <GLView
+      style={{ flex: 1 }}
+      onContextCreate={handleSetup}
+      onStartShouldSetResponder={() => true}
+      onResponderGrant={handleTouchPress}
+      onResponderRelease={handleTouchRelease}
+      onResponderMove={handleTouchMove}
+    />
+  )
 }
+
+export default App
