2fa811d0926d8688410e22c8b0a024f5b52cdacb
Tom Söderlund <tom@tomsoderlund.com>
2023-11-09 21:54:16 +0100

    Separate handleSetup/draw methods

--- App.tsx
+++ App.tsx
@@ -4,11 +4,18 @@ import { GLView } from 'expo-gl'
 import Expo2DContext, { Expo2dContextOptions } from 'expo-2d-context'
 
 export default class App extends React.Component {
+  ctx?: Expo2DContext
+
+  constructor (props: any) {
+    super(props)
+    this.ctx = undefined
+  }
+
   render (): React.ReactElement {
     return (
       <GLView
         style={{ flex: 1 }}
-        onContextCreate={this.handleGLContextCreate}
+        onContextCreate={this.handleSetup}
         onStartShouldSetResponder={() => true}
         onResponderGrant={this.handleTouchPress}
         onResponderRelease={this.handleTouchRelease}
@@ -29,36 +36,41 @@ export default class App extends React.Component {
     console.log('handleTouchMove', e.nativeEvent.locationX, e.nativeEvent.locationY)
   }
 
-  handleGLContextCreate = (gl: WebGLRenderingContext): void => {
-    const ctx = new Expo2DContext(gl as unknown as number, undefined as unknown as Expo2dContextOptions)
-    ctx.translate(50, 200)
-    ctx.scale(4, 4)
+  handleSetup = (gl: WebGLRenderingContext): void => {
+    this.ctx = new Expo2DContext(gl as unknown as number, undefined as unknown as Expo2dContextOptions)
+    this.draw()
+  }
+
+  draw = (): void => {
+    if (this.ctx === undefined) return
+    this.ctx.translate(50, 200)
+    this.ctx.scale(4, 4)
     // Head
-    ctx.fillStyle = 'grey'
-    ctx.fillRect(20, 40, 100, 100)
+    this.ctx.fillStyle = 'grey'
+    this.ctx.fillRect(20, 40, 100, 100)
     // Teeth
-    ctx.fillStyle = 'white'
-    ctx.fillRect(30, 100, 20, 30)
-    ctx.fillRect(60, 100, 20, 30)
-    ctx.fillRect(90, 100, 20, 30)
+    this.ctx.fillStyle = 'white'
+    this.ctx.fillRect(30, 100, 20, 30)
+    this.ctx.fillRect(60, 100, 20, 30)
+    this.ctx.fillRect(90, 100, 20, 30)
     // Eyes
-    ctx.beginPath()
-    ctx.arc(50, 70, 18, 0, 2 * Math.PI)
-    ctx.arc(90, 70, 18, 0, 2 * Math.PI)
-    ctx.fill()
+    this.ctx.beginPath()
+    this.ctx.arc(50, 70, 18, 0, 2 * Math.PI)
+    this.ctx.arc(90, 70, 18, 0, 2 * Math.PI)
+    this.ctx.fill()
     // Eye pupils
-    ctx.fillStyle = 'dodgerblue'
-    ctx.beginPath()
-    ctx.arc(50, 70, 8, 0, 2 * Math.PI)
-    ctx.arc(90, 70, 8, 0, 2 * Math.PI)
-    ctx.fill()
+    this.ctx.fillStyle = 'dodgerblue'
+    this.ctx.beginPath()
+    this.ctx.arc(50, 70, 8, 0, 2 * Math.PI)
+    this.ctx.arc(90, 70, 8, 0, 2 * Math.PI)
+    this.ctx.fill()
     // Antenna
-    ctx.strokeStyle = 'black'
-    ctx.beginPath()
-    ctx.moveTo(70, 40)
-    ctx.lineTo(70, 30)
-    ctx.arc(70, 20, 10, 0.5 * Math.PI, 2.5 * Math.PI)
-    ctx.stroke()
-    ctx.flush()
+    this.ctx.strokeStyle = 'black'
+    this.ctx.beginPath()
+    this.ctx.moveTo(70, 40)
+    this.ctx.lineTo(70, 30)
+    this.ctx.arc(70, 20, 10, 0.5 * Math.PI, 2.5 * Math.PI)
+    this.ctx.stroke()
+    this.ctx.flush()
   }
 }
