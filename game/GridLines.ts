3b9ab4d15a45b6cf3267a752df7a89aab8de4409
Tom Söderlund <tom@tomsoderlund.com>
2023-11-11 18:24:09 +0100

    Right wall

--- game/GridLines.ts
+++ game/GridLines.ts
@@ -1,15 +1,19 @@
 import GameObject, { GameUpdate } from './GameObject'
 
-export const MARGIN = 120
+export const MARGIN = 80
 
 export default class GridLines extends GameObject {
   draw (update: GameUpdate): void {
     this.ctx.strokeStyle = 'darkgray'
     this.ctx.lineWidth = 5
     this.ctx.beginPath()
+    // Y axis (Left wall)
     this.ctx.moveTo(MARGIN, MARGIN)
     this.ctx.lineTo(MARGIN, this.ctx.height - MARGIN) // Origo
+    // X axis (Floor)
     this.ctx.lineTo(this.ctx.width - MARGIN, this.ctx.height - MARGIN)
+    // Y axis (Right wall)
+    this.ctx.lineTo(this.ctx.width - MARGIN, MARGIN)
     this.ctx.stroke()
   }
 }
