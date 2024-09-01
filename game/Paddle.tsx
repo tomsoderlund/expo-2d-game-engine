487a40f91e00f5fb0dcae6923a714cddee23e8ea
Tom Söderlund <tom@tomsoderlund.com>
2024-09-01 17:18:19 +0200

    Update Paddle.tsx

--- game/Paddle.tsx
+++ game/Paddle.tsx
@@ -9,7 +9,16 @@ export const paddleHeight = 30
 
 const svg = Skia.SVG.MakeFromString(
   `<svg width="180" height="30" viewBox="0 0 180 30" fill="none" xmlns="http://www.w3.org/2000/svg">
-<rect width="180" height="30" rx="8" fill="#797979"/>
+<g clip-path="url(#clip0_2779_426)">
+<rect width="180" height="30" rx="8" fill="#909090"/>
+<path d="M203 30H-24L203 0V30Z" fill="black" fill-opacity="0.4"/>
+<rect x="3" y="3" width="174" height="24" rx="6" fill="#B2B2B2"/>
+</g>
+<defs>
+<clipPath id="clip0_2779_426">
+<rect width="180" height="30" rx="8" fill="white"/>
+</clipPath>
+</defs>
 </svg>`
 )
 
